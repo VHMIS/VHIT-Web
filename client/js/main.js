7a1854ec2c65f24ac5ae385c9bd0b6f4083a9a80
Le Nhat Anh <lenhatanh86@gmail.com>
2016-06-24 16:17:47 +0700

    [RS] Tìm kiếm từ API ứng dụng RS

--- client/js/main.js
+++ client/js/main.js
@@ -17375,6 +17375,9 @@ $(document).ready(function () {
  * File javascript của các trang liên quan đến nội dung NCKH
  */
 $(document).ready(function () {
+    var pub = true;
+    var domain = pub ? 'https://vhmis.viethanit.edu.vn/' : 'http://localhost/VHMIS_WWW/'
+
     // Form tra cứu lý lịch khoa khọc
     // -- Làm các thành phần select đẹp hơn
     $('#form-scv-list select[name=name]').select2({
@@ -17394,9 +17397,6 @@ $(document).ready(function () {
     })
 
     // Lấy danh sách & chi tiết lý lịch
-    var pub = false;
-    var domain = pub ? 'https://vhmis.viethanit.edu.vn/' : 'http://localhost/VHMIS_WWW/'
-
     $('div#cv_list').each(function() {
         $.get(domain + 'research/public-api/scv/list/viethan', function (data) {
             $('div#cv_list').html(data)
@@ -17408,6 +17408,40 @@ $(document).ready(function () {
         var sc = $.url('query')
         $.get(domain + 'research/public-api/scv/get-cv/' + sc, function (data) {
             $this.html(data)
+        })
+    })
+
+    // Xử lý tìm kiếm
+    $("#search-form .type-container span").on('click', function (e) {
+        $("#search-form .type-container span").removeClass('active')
+        $(this).addClass('active')
+        var searchFor = $(this).data('search-for')
+        $("#search-form .scientist-container").addClass('hidden')
+        $("#search-form .work-container").addClass('hidden')
+        $("#search-form ." + searchFor + "-container").removeClass('hidden')
+        $("#search-form").data('search-for', searchFor)
+    })
+
+    $("#search-form button").on('click', function (e) {
+        var searchFor = $("#search-form").data('searchFor')
+        if (searchFor === "work") {
+            alert("Đang triển khai")
+            return true;
+        }
+
+        var data = {
+            name: $("#search-form input[name=scientist-name]").val().trim(),
+            work: $("#search-form input[name=scientist-workfor]").val().trim(),
+            field: $("#search-form input[name=scientist-field]").val().trim()
+        }
+
+        if (data.name === "" && data.work === "" && data.field === "") {
+            alert("Vui lòng cung cấp một nội dung cần tìm kiếm")
+            $("#search-form input[name=scientist-name]").focus()
+        }
+
+        $.post(domain + 'research/public-api/scv/search/scientist', data, function (data) {
+            $("#search-result").html(data)
         })
     })
 })
