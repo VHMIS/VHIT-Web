a772bd8fe4f4f79bf29b7c97b4791ece81491cc0
Nhat Anh UW15 <lenhatanh86@gmail.com>
2017-07-19 16:05:55 +0700

    [TS] Cập nhật danh sách và form chọn trường theo tỉnh thành/quận huyện

--- client/tuyensinh/js/main.js
+++ client/tuyensinh/js/main.js
@@ -152,403 +152,14 @@ $(document).ready(function () {
         }
     }
 
-    var truongpt = {
-        "04": {
-            "001": {
-                "name": "THPT Nguy\u1ec5n Hi\u1ec1n",
-                "area": "3"
-            },
-            "002": {
-                "name": "THPT Phan Ch\u00e2u Trinh",
-                "area": "3"
-            },
-            "003": {
-                "name": "THPT Tr\u1ea7n Ph\u00fa",
-                "area": "3"
-            },
-            "006": {
-                "name": "TT GDTX, KTTH-HN&DN qu\u1eadn H\u1ea3i Ch\u00e2u",
-                "area": "3"
-            },
-            "007": {
-                "name": "THPT Th\u00e1i Phi\u00ean",
-                "area": "3"
-            },
-            "008": {
-                "name": "TT TT GDTX, KTTH-HN&DN  Thanh Kh\u00ea",
-                "area": "3"
-            },
-            "009": {
-                "name": "THPT TT Quang Trung",
-                "area": "3"
-            },
-            "028": {
-                "name": "THPT Thanh Kh\u00ea",
-                "area": "3"
-            },
-            "005": {
-                "name": "THPT Chuy\u00ean L\u00ea Qu\u00fd \u0110\u00f4n",
-                "area": "3"
-            },
-            "010": {
-                "name": "THPT Ho\u00e0ng Hoa Th\u00e1m",
-                "area": "3"
-            },
-            "011": {
-                "name": "THPT Ng\u00f4 Quy\u1ec1n",
-                "area": "3"
-            },
-            "012": {
-                "name": "TT GDTX Th\u00e0nh Ph\u1ed1",
-                "area": "3"
-            },
-            "013": {
-                "name": "TT GDTX, KTTH-HN&DN  S\u01a1n Tr\u00e0",
-                "area": "3"
-            },
-            "027": {
-                "name": "THPT T\u00f4n Th\u1ea5t T\u00f9ng",
-                "area": "3"
-            },
-            "014": {
-                "name": "THPT Ng\u0169 H\u00e0nh S\u01a1n",
-                "area": "3"
-            },
-            "015": {
-                "name": "TT GDTX, KTTH-HN&DN  Ng\u0169 H\u00e0nh S\u01a1n",
-                "area": "3"
-            },
-            "016": {
-                "name": "PTDL Hermann Gmeiner",
-                "area": "3"
-            },
-            "017": {
-                "name": "THPT Nguy\u1ec5n Tr\u00e3i",
-                "area": "3"
-            },
-            "018": {
-                "name": "THPT TT Khai Tr\u00ed",
-                "area": "3"
-            },
-            "019": {
-                "name": "TT GDTX, KTTH-HN&DN  Li\u00ean Chi\u1ec3u",
-                "area": "3"
-            },
-            "026": {
-                "name": "THPT Nguy\u1ec5n Th\u01b0\u1ee3ng Hi\u1ec1n",
-                "area": "3"
-            },
-            "021": {
-                "name": "THPT Phan Th\u00e0nh T\u00e0i",
-                "area": "2"
-            },
-            "022": {
-                "name": "THPT \u00d4ng \u00cdch Khi\u00eam",
-                "area": "2"
-            },
-            "023": {
-                "name": "THPT Ph\u1ea1m Ph\u00fa Th\u1ee9",
-                "area": "2"
-            },
-            "025": {
-                "name": "TT GDTX, KTTH-HN&DN  H\u00f2a Vang",
-                "area": "2"
-            },
-            "020": {
-                "name": "THPT H\u00f2a Vang",
-                "area": "2"
-            },
-            "024": {
-                "name": "TT GDTX, KTTH-HN&DN  C\u1ea9m L\u1ec7",
-                "area": "2"
-            },
-            "029": {
-                "name": "THPT C\u1ea9m L\u1ec7",
-                "area": "2"
-            }
-        },
-        "34": {
-            "001": {
-                "name": "THPT Duy T\u00e2n",
-                "area": "2"
-            },
-            "002": {
-                "name": "THPT Phan B\u1ed9i Ch\u00e2u",
-                "area": "2"
-            },
-            "003": {
-                "name": "THPT Tr\u1ea7n Cao V\u00e2n",
-                "area": "2"
-            },
-            "004": {
-                "name": "THPT L\u00ea Qu\u00fd \u0110\u00f4n",
-                "area": "2"
-            },
-            "005": {
-                "name": "THPT T\u01b0 th\u1ee5c H\u00e0 Huy T\u1eadp",
-                "area": "2"
-            },
-            "006": {
-                "name": "TT. GDTX t\u1ec9nh Qu\u1ea3ng Nam",
-                "area": "2"
-            },
-            "007": {
-                "name": "THPT Chuy\u00ean Nguy\u1ec5n B\u1ec9nh Khi\u00eam",
-                "area": "2"
-            },
-            "008": {
-                "name": "TT. GDTX-HN&DN  H\u1ed9i An",
-                "area": "2"
-            },
-            "009": {
-                "name": "THPT  Tr\u1ea7n Qu\u00fd C\u00e1p",
-                "area": "2"
-            },
-            "010": {
-                "name": "THPT Chuy\u00ean L\u00ea Th\u00e1nh T\u00f4ng",
-                "area": "2"
-            },
-            "011": {
-                "name": "PTDT N\u1ed9i tr\u00fa t\u1ec9nh Qu\u1ea3ng Nam",
-                "area": "2"
-            },
-            "012": {
-                "name": "THPT Nguy\u1ec5nTr\u00e3i",
-                "area": "2"
-            },
-            "065": {
-                "name": "THPT  Tr\u1ea7n H\u01b0ng \u0110\u1ea1o",
-                "area": "2"
-            },
-            "013": {
-                "name": "THPT S\u00e0o Nam",
-                "area": "2NT"
-            },
-            "014": {
-                "name": "THPT L\u00ea H\u1ed3ng Phong",
-                "area": "2NT"
-            },
-            "015": {
-                "name": "TT. GDTX-HN  Duy Xuy\u00ean",
-                "area": "2NT"
-            },
-            "016": {
-                "name": "THPT Nguy\u1ec5n Hi\u1ec1n",
-                "area": "1"
-            },
-            "017": {
-                "name": "THPT Nguy\u1ec5n Duy Hi\u1ec7u",
-                "area": "2NT"
-            },
-            "018": {
-                "name": "THPT Ho\u00e0ng Di\u1ec7u",
-                "area": "2NT"
-            },
-            "019": {
-                "name": "THPT Ph\u1ea1m Ph\u00fa Th\u1ee9",
-                "area": "2NT"
-            },
-            "020": {
-                "name": "THPT L\u01b0\u01a1ng Th\u1ebf Vinh",
-                "area": "2NT"
-            },
-            "021": {
-                "name": "THPT Nguy\u1ec5n Khuy\u1ebfn",
-                "area": "2NT"
-            },
-            "022": {
-                "name": "TT. GDTX-HN  \u0110i\u1ec7n B\u00e0n",
-                "area": "2NT"
-            },
-            "066": {
-                "name": "Tr\u01b0\u1eddng PT nhi\u1ec1u c\u1ea5p h\u1ecdc Ho\u00e0ng Sa",
-                "area": "2NT"
-            },
-            "067": {
-                "name": "Tr\u01b0\u1eddng PT nhi\u1ec1u c\u1ea5p h\u1ecdc Qu\u1ea3ng \u0110\u00f4ng",
-                "area": "2NT"
-            },
-            "023": {
-                "name": "THPT  Hu\u1ef3nh Ng\u1ecdc Hu\u1ec7",
-                "area": "2NT"
-            },
-            "024": {
-                "name": "THPT  Chu V\u0103n An",
-                "area": "1"
-            },
-            "025": {
-                "name": "THPT  \u0110\u1ed7 \u0110\u0103ng Tuy\u1ec3n",
-                "area": "2NT"
-            },
-            "026": {
-                "name": "THPT  L\u01b0\u01a1ng Th\u00fac K\u1ef3",
-                "area": "2NT"
-            },
-            "027": {
-                "name": "TT. GDTX-HN&DN  \u0110\u1ea1i L\u1ed9c",
-                "area": "2NT"
-            },
-            "029": {
-                "name": "THPT  Qu\u1ebf S\u01a1n",
-                "area": "2NT"
-            },
-            "030": {
-                "name": "THPT  Nguy\u1ec5n V\u0103n C\u1eeb",
-                "area": "2NT"
-            },
-            "032": {
-                "name": "THPT  Tr\u1ea7n \u0110\u1ea1i Ngh\u0129a",
-                "area": "2NT"
-            },
-            "033": {
-                "name": "TT. GDTX-HN&DN  Qu\u1ebf S\u01a1n",
-                "area": "2NT"
-            },
-            "034": {
-                "name": "THPT TT Ph\u1ea1m V\u0103n \u0110\u1ed3ng",
-                "area": "2NT"
-            },
-            "035": {
-                "name": "THPT Hi\u1ec7p \u0110\u1ee9c",
-                "area": "1"
-            },
-            "036": {
-                "name": "TT. GDTX-HN&DN  Hi\u1ec7p \u0110\u1ee9c",
-                "area": "1"
-            },
-            "063": {
-                "name": "THPT  Tr\u1ea7n Ph\u00fa",
-                "area": "1"
-            },
-            "028": {
-                "name": "THPT  H\u00f9ng V\u01b0\u01a1ng",
-                "area": "2NT"
-            },
-            "037": {
-                "name": "THPT  Ti\u1ec3u La",
-                "area": "2NT"
-            },
-            "038": {
-                "name": "THPT  Nguy\u1ec5n Th\u00e1i B\u00ecnh",
-                "area": "1"
-            },
-            "039": {
-                "name": "THPT Th\u00e1i Phi\u00ean",
-                "area": "2NT"
-            },
-            "040": {
-                "name": "THPT  L\u00fd T\u1ef1 Tr\u1ecdng",
-                "area": "2NT"
-            },
-            "041": {
-                "name": "TT. GDTX-HN&DN  Th\u0103ng B\u00ecnh",
-                "area": "2NT"
-            },
-            "042": {
-                "name": "THPT  N\u00fai Th\u00e0nh",
-                "area": "2NT"
-            },
-            "043": {
-                "name": "THPT  Cao B\u00e1 Qu\u00e1t",
-                "area": "1"
-            },
-            "044": {
-                "name": "THPT  Nguy\u1ec5n Hu\u1ec7",
-                "area": "2NT"
-            },
-            "045": {
-                "name": "TT. GDTX-HN  N\u00fai Th\u00e0nh",
-                "area": "2NT"
-            },
-            "046": {
-                "name": "THPT  Hu\u1ef3nh Th\u00fac Kh\u00e1ng",
-                "area": "1"
-            },
-            "047": {
-                "name": "THPT  Phan Ch\u00e2u Trinh",
-                "area": "1"
-            },
-            "048": {
-                "name": "TT. GDTX-HN&DN  Ti\u00ean Ph\u01b0\u1edbc",
-                "area": "1"
-            },
-            "049": {
-                "name": "THPT B\u1eafc Tr\u00e0 My",
-                "area": "1"
-            },
-            "050": {
-                "name": "TT. GDTX-HN B\u1eafc Tr\u00e0 My",
-                "area": "1"
-            },
-            "062": {
-                "name": "PTDT N\u1ed9i tr\u00fa N\u01b0\u1edbc Oa",
-                "area": "1"
-            },
-            "051": {
-                "name": "THPT  Quang Trung",
-                "area": "1"
-            },
-            "064": {
-                "name": "THPT  \u00c2u C\u01a1",
-                "area": "1"
-            },
-            "052": {
-                "name": "THPT Nam Giang",
-                "area": "1"
-            },
-            "060": {
-                "name": "TT.GDTX Nam Giang",
-                "area": "1"
-            },
-            "068": {
-                "name": "THPT  Nguy\u1ec5n V\u0103n Tr\u1ed7i",
-                "area": "1"
-            },
-            "053": {
-                "name": "THPT Kh\u00e2m \u0110\u1ee9c",
-                "area": "1"
-            },
-            "059": {
-                "name": "TT GDTX-HN&DN Ph\u01b0\u1edbc S\u01a1n",
-                "area": "1"
-            },
-            "069": {
-                "name": "Ph\u1ed5 th\u00f4ng D\u00e2n t\u1ed9c N\u1ed9i tr\u00fa Ph\u01b0\u1edbc S\u01a1n",
-                "area": "1"
-            },
-            "056": {
-                "name": "THPT Nam Tr\u00e0 My",
-                "area": "1"
-            },
-            "058": {
-                "name": "TT.GDTX-HN Nam Tr\u00e0 My",
-                "area": "1"
-            },
-            "072": {
-                "name": "Ph\u1ed5 th\u00f4ng D\u00e2n t\u1ed9c n\u1ed9i tr\u00fa Nam Tr\u00e0 My",
-                "area": "1"
-            },
-            "057": {
-                "name": "THPT T\u00e2y Giang",
-                "area": "1"
-            },
-            "054": {
-                "name": "THPT Tr\u1ea7n V\u0103n D\u01b0",
-                "area": "2NT"
-            },
-            "055": {
-                "name": "TT GDTX-HN&DN  Ph\u00fa Ninh",
-                "area": "2NT"
-            },
-            "061": {
-                "name": "THPT Nguy\u1ec5n D\u1ee5c",
-                "area": "2NT"
-            },
-            "031": {
-                "name": "THPT N\u00f4ng S\u01a1n",
-                "area": "1"
-            }
-        }
+    var truongpt = {}
+
+    var makePDistrictSelect = function (data) {
+        var html = ''
+        $.each(data, function (index, value) {
+            html += '<option value="' + index + '">' + value.name + '</option>'
+        })
+        $('form#xettuyen select[name=gradute_school_k12_district]').html(html).prop('disabled', false)
     }
 
     var makeSchoolSelect = function (data) {
@@ -556,37 +167,64 @@ $(document).ready(function () {
         $.each(data, function (index, value) {
             html += '<option value="' + index + '">' + value.name + '</option>'
         })
-        $('form#xettuyen select[name=school], form#xettuyen_ne select[name=school]').html(html).prop('disabled', false)
+        $('form#xettuyen select[name=gradute_school_k12_school]').html(html).prop('disabled', false)
     }
 
-    $('form#xettuyen select[name=province], form#xettuyen_ne select[name=province]').on('change', function (e) {
+    $('form#xettuyen select[name=gradute_school_k12_province]').on('change', function (e) {
         var me = $(this)
-        var pro = me.val();
-        if (pro == "") {
-            $('form#xettuyen select[name=school], form#xettuyen_ne select[name=school]').html('<option value="0">Chọn trường</option>').prop('disabled', true)
+        var pro = me.val()
+        if (pro === '') {
+            $('form#xettuyen select[name=gradute_school_k12_district]').html('<option value="0">Chọn quận huyện</option>').prop('disabled', true)
+            $('form#xettuyen select[name=gradute_school_k12_school]').html('<option value="0">Chọn trường</option>').prop('disabled', true)
         } else {
-            if (typeof truongpt[pro] == 'undefined') {
-                $('form#xettuyen select[name=school], form#xettuyen_ne select[name=school]').html('<option value="0">Đang tải</option>').prop('disabled', true)
-                $('form#xettuyen select[name=province], form#xettuyen_ne select[name=province]').prop('disabled', true)
+            if (typeof truongpt[pro] === 'undefined') {
+                $('form#xettuyen select[name=gradute_school_k12_district]').html('<option value="0">Đang tải</option>').prop('disabled', true)
+                $('form#xettuyen select[name=gradute_school_k12_school]').html('<option value="0">Đang tải</option>').prop('disabled', true)
+                $('form#xettuyen select[name=gradute_school_k12_province]').prop('disabled', true)
                 $.getJSON('data/pt_' + pro + '.json', function (data) {
                     truongpt[pro] = data
-                    makeSchoolSelect(data)
-                    $('form#xettuyen select[name=province], form#xettuyen_ne select[name=province]').prop('disabled', false)
-                    $('form#xettuyen select[name=school], form#xettuyen_ne select[name=school]').trigger('change')
-                });
+                    makePDistrictSelect(data)
+                    //makeSchoolSelect(data)
+                    $('form#xettuyen select[name=gradute_school_k12_province]').prop('disabled', false)
+                    $('form#xettuyen select[name=gradute_school_k12_district]').trigger('change')
+                })
             } else {
-                makeSchoolSelect(truongpt[pro])
-                $('form#xettuyen select[name=school], form#xettuyen_ne select[name=school]').trigger('change')
+                makePDistrictSelect(truongpt[pro])
+                //makeSchoolSelect(truongpt[pro])
+                $('form#xettuyen select[name=gradute_school_k12_district]').trigger('change')
             }
         }
     })
 
     var areas = {
-        '1': 'Khu vực 1',
-        '2': 'Khu vực 2',
-        '2NT': 'Khu vực 2-NT',
-        '3': 'Khu vực 3',
+        '1': 'KV 1',
+        '2': 'KV 2',
+        '2NT': 'KV 2-NT',
+        '3': 'KV 3'
     }
+
+    $('form#xettuyen select[name=gradute_school_k12_district]').on('change', function (e) {
+        var me = $(this)
+        var pro = $('form#xettuyen select[name=gradute_school_k12_province]').val()
+        var district = me.val()
+        makeSchoolSelect(truongpt[pro][district]['school'])
+        $('form#xettuyen select[name=gradute_school_k12_school]').trigger('change')
+    })
+
+    $('form#xettuyen select[name=gradute_school_k12_school]').on('change', function (e) {
+        var me = $(this)
+        var school = me.val()
+        if (school === '') {
+            $('form#xettuyen input[name=gradute_school_k12_priority_area], form#xettuyen_ne input[name=priority_area]').val('')
+            $('form#xettuyen input[name=gradute_school_k12_priority_area_text], form#xettuyen_ne input[name=priority_area_text]').val('Khu vực')
+        } else {
+            var pro = $('form#xettuyen select[name=gradute_school_k12_province]').val()
+            var district = $('form#xettuyen select[name=gradute_school_k12_district]').val()
+            var area = truongpt[pro][district]['school'][school]['area']
+            $('form#xettuyen input[name=gradute_school_k12_priority_area], form#xettuyen_ne input[name=priority_area').val(area)
+            $('form#xettuyen input[name=gradute_school_k12_priority_area_text], form#xettuyen_ne input[name=priority_area_text]').val(areas[area])
+        }
+    })
 
     var majors = {
         cd: {
@@ -662,20 +300,6 @@ $(document).ready(function () {
 
     $('form#xettuyen select[name=major_1]').on('change', function (e) {
         makeDisableMajor()
-    })
-
-    $('form#xettuyen select[name=school], form#xettuyen_ne select[name=school]').on('change', function (e) {
-        var me = $(this)
-        var school = me.val()
-        if (school == "") {
-            $('form#xettuyen input[name=priority_area], form#xettuyen_ne input[name=priority_area]').val('')
-            $('form#xettuyen input[name=priority_area_text], form#xettuyen_ne input[name=priority_area_text]').val('Khu vực')
-        } else {
-            var pro = $('form#xettuyen select[name=province], form#xettuyen_ne select[name=province]').val()
-            var area = truongpt[pro][school]['area']
-            $('form#xettuyen input[name=priority_area], form#xettuyen_ne input[name=priority_area').val(area)
-            $('form#xettuyen input[name=priority_area_text], form#xettuyen_ne input[name=priority_area_text]').val(areas[area])
-        }
     })
 
     var diachihk = {}
