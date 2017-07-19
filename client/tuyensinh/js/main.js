1d4e591465733c9122cc75f2146fa441e9699504
Le Nhat Anh <lenhatanh86@gmail.com>
2017-07-20 06:40:49 +0700

    [TS] Cập nhật form đk tuyển sinh

--- client/tuyensinh/js/main.js
+++ client/tuyensinh/js/main.js
@@ -436,8 +436,8 @@ $(document).ready(function () {
         var me = $(this)
         var data = me.serialize();
         me.find('button').prop('disabled', true);
-        $.post('https://vhmis.viethanit.edu.vn/education/public-api/admission/add', data, function (data) {
-        //$.post('http://localhost/VHMIS_WWW/education/public-api/admission/add', data, function (data) {
+        //$.post('https://vhmis.viethanit.edu.vn/education/public-api/admission/add', data, function (data) {
+        $.post('http://localhost/VHMIS_WWW/education/public-api/admission/add', data, function (data) {
             if (data.error == '0') {
                 alert('Cảm ơn bạn đã đăng ký xét tuyển vào trường Việt Hàn, chúng tôi sẽ liên lạc và thông báo kết quả sớm với bạn.')
                 me[0].reset();
