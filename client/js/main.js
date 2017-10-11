89a0d89db1271751ff2f5bff5da967c23b705f45
Nhat Anh UW15 <lenhatanh86@gmail.com>
2017-10-11 16:06:45 +0700

    [TV] 0.1

--- client/js/main.js
+++ client/js/main.js
@@ -16653,9 +16653,47 @@ $(document).ready(function () {
   var domain = pub ? 'https://vhmis.viethanit.edu.vn/' : 'http://localhost/'
 
   // Sach moi
-  $('div#list-new-docs').each(function () {
+  $('#biblios').each(function () {
     $.get(domain + 'library/public-api/lastest-biblios', function (data) {
       $('div#list-new-docs').html(data)
+    })
+  })
+
+  // Loại sách
+  var getBibliosByType = function(type, container) {
+    $.get(domain + 'library/public-api/biblio/search', {'k': 'dc.kohaitemtype', 'q': type}, function (data) {
+      $(container).html(data)
+    })
+  }
+
+  // test
+  $('#search-form form').on('submit', function (e) {
+    e.preventDefault();
+    var $this = $(this)
+    var data = $this.serialize()
+
+    $.get(domain + 'library/public-api/biblio/search', data, function (data) {
+      $('#biblios').html(data)
+    })
+  })
+
+  $('#search-form').on('click', 'li a', function (e) {
+    e.preventDefault()
+    var type = $(this).data('type')
+    var container = '#biblios'
+    getBibliosByType(type, container)
+  })
+
+  $('#biblios').on('click', 'a.page-search', function (e) {
+    e.preventDefault()
+    var data = {
+      'p': $(this).data('page'),
+      'k': $(this).data('keyword'),
+      'q': $(this).data('query')
+    }
+
+    $.get(domain + 'library/public-api/biblio/search', data, function (data) {
+      $('#biblios').html(data)
     })
   })
 })
